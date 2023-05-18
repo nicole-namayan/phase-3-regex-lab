a2984a1502ccb0caebe66f7da894ce1dcf59bb80
nicole <nicolenamayan133@gmail.com>
2023-05-18 11:34:25 +0300

    done

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,18 @@
 def starts_with_a_vowel?(word)
-
+    word.match?(/\A[aeiou]/i)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+   text.scan(/\bun\w+ing\b/i)
 end
-
 def words_five_letters_long(text)
-
+    text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    /^[A-Z].*[[:punct:]]$/ === text
 end
 
 def valid_phone_number?(phone)
-
+    /^\d{10}$/ === phone.gsub(/\D/, '')
 end
